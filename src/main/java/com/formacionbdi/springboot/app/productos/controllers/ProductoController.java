189f6c9bec1d327b7f90bda67e7d6690609d9a57
basiliorouco <basiliorouco@gmail.com>
2019-09-14 00:23:38 +0200

    no message

---
+++ src/main/java/com/formacionbdi/springboot/app/productos/controllers/ProductoController.java
@@ -0,0 +1,52 @@
+package com.formacionbdi.springboot.app.productos.controllers;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.formacionbdi.springboot.app.productos.models.entity.Producto;
+import com.formacionbdi.springboot.app.productos.models.service.IProductoService;
+
+@RestController()
+public class ProductoController {
+	private Logger log = LoggerFactory.getLogger(this.getClass());
+	
+	@Autowired
+	private Environment env;
+	
+	@Value("${server.port}")
+	private Integer port;
+	
+	@Autowired
+	private IProductoService productoService;
+	
+	@GetMapping(path = "/listar")
+	public List<Producto> listar(){
+		log.info("Obteniendo listado de productos");
+		return productoService.findAll().stream().map(producto -> {
+			//producto.setPort(Integer.parseInt(env.getProperty("local.server.port")));
+			producto.setPort(port);
+			return producto;
+		}).collect(Collectors.toList());
+	}
+	
+	@GetMapping(path= "/producto/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public Producto detalle(@PathVariable Long id) {
+		log.info("Obteniendo el producto con ID: "+id);
+		Producto producto = productoService.findById(id);
+		//producto.setPort(Integer.parseInt(env.getProperty("local.server.port")));
+		producto.setPort(port);
+		return producto;
+	}
+	
+
+}
---
+++ src/main/resources/import.sql
@@ -0,0 +1,9 @@
+insert into productos(nombre,precio,create_at) values ('Panasonic',800,NOW());
+insert into productos(nombre,precio,create_at) values ('Sony',850,NOW());
+insert into productos(nombre,precio,create_at) values ('Apple',670,NOW());
+insert into productos(nombre,precio,create_at) values ('HP',600,NOW());
+insert into productos(nombre,precio,create_at) values ('Adidas',500,NOW());
+insert into productos(nombre,precio,create_at) values ('Nike',400,NOW());
+insert into productos(nombre,precio,create_at) values ('Reebook',10,NOW());
+insert into productos(nombre,precio,create_at) values ('NewBalance',80,NOW());
+insert into productos(nombre,precio,create_at) values ('Bianchi',300,NOW());
