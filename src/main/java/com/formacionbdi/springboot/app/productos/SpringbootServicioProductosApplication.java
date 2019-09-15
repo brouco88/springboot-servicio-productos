b5ce2b643ab7a325e1546e08535db0741004bf3b
basiliorouco <basiliorouco@gmail.com>
2019-09-15 22:13:46 +0200

    Modificaciones para consumir nuevos metodos Productos

--- src/main/java/com/formacionbdi/springboot/app/productos/SpringbootServicioProductosApplication.java
+++ src/main/java/com/formacionbdi/springboot/app/productos/SpringbootServicioProductosApplication.java
@@ -1,11 +1,15 @@
 package com.formacionbdi.springboot.app.productos;
 
+import javax.persistence.Entity;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
 @SpringBootApplication
+@EntityScan("com.formacionbdi.springboot.app.commons.models.entity")
 public class SpringbootServicioProductosApplication {
 
 	public static void main(String[] args) {
