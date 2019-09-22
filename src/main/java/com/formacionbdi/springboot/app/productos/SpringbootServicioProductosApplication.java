35da14f0e65bee73572429fb08e8e40f119fc07c
basiliorouco <basiliorouco@gmail.com>
2019-09-22 22:06:37 +0200

    j

--- src/main/java/com/formacionbdi/springboot/app/productos/SpringbootServicioProductosApplication.java
+++ src/main/java/com/formacionbdi/springboot/app/productos/SpringbootServicioProductosApplication.java
@@ -1,7 +1,5 @@
 package com.formacionbdi.springboot.app.productos;
 
-import javax.persistence.Entity;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
